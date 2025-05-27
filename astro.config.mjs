787fa8e98fd3a831a1fe623f67b75eb71cc4c7ef
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-05-27 16:12:17 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bamo0507.github.io/Portfolio/',
-    base: '/Portafolio/',
     vite: {
         plugins: [tailwindcss()]
     }
